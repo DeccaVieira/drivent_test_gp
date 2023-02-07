492d71db12e1c568d731c5bec1dba9caa28bb8ed
Hugo junior <hjunioreng@gmail.com>
2023-02-07 19:41:21 -0300

    Controller Get-booking

---
+++ src/controllers/booking-controller.ts
@@ -0,0 +1,15 @@
+import { Response } from 'express';
+import { AuthenticatedRequest } from '@/middlewares';
+//import bookingService from "@/services/booking-service";
+import httpStatus from 'http-status';
+
+export async function getBooking(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+
+  try {
+    const bookingByUser = await bookingService.getBooking(Number(userId));
+    return res.status(httpStatus.OK).send(bookingByUser);
+  } catch (error) {
+    return res.sendStatus(httpStatus.NOT_FOUND);
+  }
+}
